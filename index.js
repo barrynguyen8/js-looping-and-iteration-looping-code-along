ce998513ab685b6f28af3429b96666371a46cd8f
Barry Nguyen <barry.tuan.nguyen@gmail.com>
2021-03-08 15:55:04 +1100

    assignment submitted

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+const nameList = ["Ada", "Brendan", "Ali"];
+
+function writeCards(nameList, eventName) {
+    const newList = [];
+    for (let i =0; i <nameList.length; i++) {
+        newList.push(`Thank you, ${nameList[i]}, for the wonderful ${eventName} gift!`)
+    }
+    return newList 
+}
+
+function countDown(number) {
+    let i = number;
+    while (i >= 0) {
+        console.log(i); i--;
+    }
+}
